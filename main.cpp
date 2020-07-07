a48e2b0bfa01079ea285d4165620a28d6c33b6f5
katepang13 <liuyhpangpang@gmail.com>
2020-07-07 21:12:23 +0800

    Sending Rays Into the Scene

--- main.cpp
+++ main.cpp
@@ -1,20 +1,46 @@
+#include "vec3.h"
 #include "color.h"
+#include "ray.h"
 
 #include <iostream>
 
+/*
+The ray_color(ray) function linearly blends white and blue depending on 
+the height of the y coordinate after scaling the ray direction to unit length (so −1.0<y<1.0). 
+Because we're looking at the y height after normalizing the vector, 
+you'll notice a horizontal gradient to the color in addition to the vertical gradient.
+*/
+color ray_color(const ray&r) {
+    vec3 unit_direction = unit_vector(r.direction());
+    auto t = 0.5*(unit_direction.y() + 1.0);
+    return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
+}
+
 int main() {
-	const int image_width = 256;
-    const int image_height = 256;
+    const auto aspect_ratio = 16.0 / 9.0;
+	const int image_width = 384;
+    const int image_height = static_cast<int>(image_width/aspect_ratio);
 
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+    auto viewport_height = 2.0;
+    auto viewport_weight = aspect_ratio*viewport_height;
+    auto focal_length = 1.0;
+    
+    auto origin = point3(0, 0, 0); 
+    auto horizontal = vec3(viewport_weight, 0, 0);
+    auto vertical = vec3(0, viewport_height, 0);
+    auto lower_left_corner = origin - horizontal/2 - vertical/2 - vec3(0, 0, focal_length);
 
     for( int j = image_height-1; j >=0; --j ) {
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for(int i = 0; i<image_width; ++i) {
-            auto r = double(i) / (image_width-1);
-            auto g = double(j) / (image_height-1);
-            auto b = 0.25;
-            write_color(std::cout, color(r, g, b));
+            auto u = double(i) / (image_width-1);
+            auto v = double(j) / (image_height-1);
+            point3 pixel = lower_left_corner + u*horizontal + v*vertical;//屏幕上的点的二维坐标转换为三维坐标;
+            vec3 direction = pixel - origin;//原点(视点)连到屏幕上一点，即是该点的光线方向;
+            ray r(origin, direction);
+            write_color(std::cout, ray_color(r) );
         }
     }
     std::cerr << "\nDone.\n";
---
+++ ray.h
@@ -0,0 +1,34 @@
+/*
+The one thing that all ray tracers have is a ray class and a computation of what color is seen along a ray.
+ Let’s think of a ray as a function P(t)=A+tb. Here P is a 3D position along a line in 3D. 
+ A is the ray origin and b is the ray direction. The ray parameter t is a real number (double in the code). 
+ Plug in a different t and P(t) moves the point along the ray. 
+ Add in negative t values and you can go anywhere on the 3D line. 
+ For positive t, you get only the parts in front of A, and this is what is often called a half-line or ray.
+*/
+
+#ifndef RAT_H
+#define RAT_H
+
+#include "vec3.h"
+
+class ray {
+public:
+    ray() {};
+    ray(const point3& origin, const vec3& direction)
+        : orig(origin), dir(direction) {
+    }
+
+    point3 origin() const {return orig; }
+    vec3 direction() const {return dir; }
+
+    point3 at(double t) const {
+        return orig + t*dir;
+    }
+
+public:
+    point3 orig;
+    vec3 dir;
+};
+
+#endif

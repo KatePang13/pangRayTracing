f00b02a357b4c01db9c1cd5aecb6ef78e099b069
katepang13 <liuyhpangpang@gmail.com>
2020-07-08 21:22:21 +0800

    11.2 Positioning and Orienting the Camera

--- main.cpp
+++ main.cpp
@@ -42,11 +42,13 @@ int main() {
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     hittable_list world;
-    auto R = cos(pi/4);
-    world.add(make_shared<sphere>(point3(-R,0,-1), R, make_shared<lambertian>(color(0, 0, 1))));
-    world.add(make_shared<sphere>(point3( R,0,-1), R, make_shared<lambertian>(color(1, 0, 0))));
+    world.add(make_shared<sphere>(point3(0,0,-1), 0.5, make_shared<lambertian>(color(.1, .2, .5))));
+world.add(make_shared<sphere>(point3(0,-100.5,-1), 100, make_shared<lambertian>(color(.8,.8,0.))));
+world.add(make_shared<sphere>(point3(1,0,-1), 0.5, make_shared<metal>(color(.8, .6, .2), 0.3)));
+world.add(make_shared<sphere>(point3(-1,0,-1), 0.5, make_shared<dielectric>(1.5)));
+world.add(make_shared<sphere>(point3(-1,0,-1), -0.45, make_shared<dielectric>(1.5)));
 
-    camera cam(90, double(image_width/image_height));
+    camera cam(point3(-2,2,1), point3(0,0,-1), vec3(0,1,0), 90, aspect_ratio);
 
     for( int j = image_height-1; j >=0; --j ) {
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;

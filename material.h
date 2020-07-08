a78d1f4a3c9bfa3dbdbc144cc821f16a534f35c3
katepang13 <liuyhpangpang@gmail.com>
2020-07-08 20:44:38 +0800

    10.3 Total Internal Reflection

--- material.h
+++ material.h
@@ -7,15 +7,15 @@
 
 class meterial {
 public:
-    virtual bool scatter(const ray& r_in, hit_record& rec, color& attenuation, ray& sattered) const =0;
+    virtual bool scatter(const ray& r_in, hit_record& rec, color& attenuation, ray& scattered) const =0;
 };
 
 class lambertian : public meterial {
 public:
     lambertian(const color& a) : albedo(a) {};
-    virtual bool scatter(const ray& r_in, hit_record& rec, color& attenuation, ray& sattered) const {
+    virtual bool scatter(const ray& r_in, hit_record& rec, color& attenuation, ray& scattered) const {
         vec3 scatter_direction = rec.normal + random_unit_vector();
-        sattered = ray( rec.p, scatter_direction );
+        scattered = ray( rec.p, scatter_direction );
         attenuation = albedo;
         return true;
     } 
@@ -28,16 +28,51 @@ class metal : public meterial {
 public:
     metal(const color& a) : albedo(a), fuzz(1.0) {};
     metal(const color& a, double f) : albedo(a), fuzz(f<1 ? f:1) {};
-    virtual bool scatter(const ray& r_in, hit_record& rec, color& attenuation, ray& sattered) const {
+    virtual bool scatter(const ray& r_in, hit_record& rec, color& attenuation, ray& scattered) const {
         vec3 reflect_direction = reflect( unit_vector(r_in.direction()), rec.normal );
-        sattered = ray( rec.p, reflect_direction + fuzz*random_in_unit_sphere() );
+        scattered = ray( rec.p, reflect_direction + fuzz*random_in_unit_sphere() );
         attenuation = albedo;
-        return  dot(sattered.direction(), rec.normal) > 0;
+        return  dot(scattered.direction(), rec.normal) > 0;
     } 
 
 public:
     color albedo;
     double fuzz;
+};
+
+class dielectric : public meterial {
+public:
+    dielectric(double ri) : ref_idx(ri) {}
+    virtual bool scatter(const ray& r_in, hit_record& rec, color& attenuation, ray& scattered) const {
+        attenuation = color(1.0, 1.0, 1.0);
+        double etai_over_etat = 1.0; 
+        if( rec.front_face ) {
+            etai_over_etat = 1.0/ref_idx;
+        } else {
+            etai_over_etat = ref_idx;
+        }
+
+        vec3 unit_direction = unit_vector(r_in.direction());
+        
+        double cos_theta = fmin(dot(-unit_direction, rec.normal), 1.0);
+        double sin_theta = sqrt( 1.0 - cos_theta*cos_theta );
+        
+        //反射;
+        if( etai_over_etat * sin_theta > 1.0 ) {
+            vec3 refrected = reflect(unit_direction, rec.normal);
+            scattered = ray(rec.p, refrected);
+            return true;
+        }
+
+        //折射;
+        vec3 refracted = refract(unit_direction, rec.normal, etai_over_etat);
+        scattered = ray(rec.p, refracted);
+        return true;
+    }
+    
+
+public:
+    double ref_idx;    
 };
 
 #endif
